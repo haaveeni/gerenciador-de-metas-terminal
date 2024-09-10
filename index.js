e2a84af6737725ace5467dab71af6469c4fa666b
Veridiana Lucena <veridianalucena@gmail.com>
2024-09-10 18:41:42 -0300

    updated index

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
-// hello world
-let mensagem = 2
-console.log(mensagem)
-
-// arrays, objetos
-// let metas = ["veri", "alo"]
-
-// console.log(metas[1] + ", " + metas[0])
-
-let meta = {
-  value: 'ler um livro por mês',
-  checked: false,
-  // log: (info) => {
-  //   console.log(info)
-  // }
+const start = () => {
+  let opcao = "sair"
+  switch (opcao) {
+    case "cadastrar":
+      console.log("vamos cadastrar")
+      break
+    case "listar":
+      console.log("vamos listar")
+      break
+    case "sair":
+      return
+  }
 }
 
-let metas = [
-  meta,//ler um livro por mes, meta[0]
-  {
-    value: "estudar inglês 15 minutos todos os dias",//meta[1]
-    checked: false,
-  }
-]
-
-// console.log(meta.checked);
-
-// function // arrow function
-// const criarMeta = () => {} 
-
-// function criarMeta() {}
-
-// meta.value = "Não é mais ler um livro"
-// meta.log(meta.value)
+start()
